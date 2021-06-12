d884959f6be1b39205593f6c0ad52fd6d9712d58
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2021-06-12 15:44:11 +0200

    topbar show counter of product in wishlist now

--- components/TopBarMenu/index.tsx
+++ components/TopBarMenu/index.tsx
@@ -3,9 +3,12 @@ import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { IoHeartOutline, IoBasketOutline } from "react-icons/io5";
 import Link from "next/link";
 import { useCart } from "storage/Cart";
+import { useWishlist } from "@storage/Wishlist";
 
 const TopBarMenu = () => {
-  const { count } = useCart();
+  const { count: productInCart } = useCart();
+  const { count: productInWishList } = useWishlist();
+  
   return (
     <TopBarStyle top>
       <div className="menu">
@@ -20,13 +23,13 @@ const TopBarMenu = () => {
         <Link href="/wishlist" passHref>
           <a className="icon-and-count">
             <IoHeartOutline size="1.4rem" />
-            <span>{count}</span>
+            <span>{productInWishList}</span>
           </a>
         </Link>
         <Link href="/cart" passHref>
           <a className="icon-and-count">
             <IoBasketOutline size="1.4rem" />
-            <span>{count}</span>
+            <span>{productInCart}</span>
           </a>
         </Link>
       </div>
