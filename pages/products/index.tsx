ede7e4accee530d2c587b98391a5afaf2af06b01
Juanan Rodriguez <hola@juananrodriguez.es>
2021-06-26 03:24:57 +0200

    first attempt at infinite loading in the product listing by scrolling

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -2,12 +2,57 @@ import { ProductCardList } from "@components";
 import { getApiPath } from "utils/getApiPath";
 import { ProductList } from "@domainTypes/Product";
 import { GetStaticProps } from "next";
+import { useEffect } from "react";
+import { useState } from "react";
 
 type Props = {
   data: ProductList;
 };
 const EntryPoint = ({ data }: Props) => {
-  return <ProductCardList products={data} />;
+  const [products, setProducts] = useState(data);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    let options = {
+      root: null,
+      rootMargin: "200px",
+      threshold: 0,
+      trackVisibility: true,
+      delay: 100,
+    };
+
+    if(!isFetching){
+      console.log('creado')
+    const getProducts = async (): Promise<void> => {
+      console.log("products dentro", products.length);
+      setIsFetching(true);
+      
+      const url = getApiPath({ dirname: __dirname, url: "products" });
+      const res = await fetch(`${url}?offset=${products.length}`);
+      const data = await res.json();
+
+      setProducts((products) => [...products, ...data]);
+      setIsFetching(false);
+    };
+
+    let observer = new IntersectionObserver(
+      (e) => {e[0]?.isIntersecting && getProducts()},
+      options
+    );
+
+    const nodeToObserve = document.querySelector("#end-of-product-list");
+    nodeToObserve && observer.observe(nodeToObserve);
+
+    return ()=>observer.disconnect();
+    }
+  }, [products, isFetching]);
+
+  return (
+    <>
+      <ProductCardList products={products} />
+      <div id="end-of-product-list" />
+    </>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
